9244036c76f1a1a0df6a879e70ec308b1e31d597
Nikh <116083086+craftyntuz@users.noreply.github.com>
2023-09-14 05:30:02 +0000

    Updated 4 files

--- src/assets/components/navbar.jsx
+++
@@ -1,8 +0,0 @@
-import react from 'react';
-import reactdom from 'react-dom';
-
-export default navbar(){
- return( <div class="navbar">
- 
-  </div>)
-}
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Navbar() {
+  return (
+    <div className="navbar">
+      <img src="https://creative.starbucks.com/" alt="starbucks" />
+    </div>
+  );
+}
+
+export default Navbar();
